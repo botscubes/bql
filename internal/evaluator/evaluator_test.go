a6680b1f321be7267cbc155bf50b029fda081901
kr-ilya <kryuchkov-ilya@bk.ru>
2024-03-22 20:08:15 +0300

    Add TestEvalBooleanExpresion

--- internal/evaluator/evaluator_test.go
+++ internal/evaluator/evaluator_test.go
@@ -23,6 +23,21 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestEvalBooleanExpresion(t *testing.T) {
+	tests := []struct {
+		input    string
+		excepted bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, test := range tests {
+		ev := getEvaluated(test.input)
+		testBoolean(t, ev, test.excepted)
+	}
+}
+
 func getEvaluated(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -40,5 +55,16 @@ func testInteger(t *testing.T, obj object.Object, expected int64) {
 	if res.Value != expected {
 		t.Errorf("obj wrong value. got: %d expected: %d", res.Value, expected)
 	}
+}
 
+func testBoolean(t *testing.T, obj object.Object, expected bool) {
+	res, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("obj not Boolean got:%+v", obj)
+		return
+	}
+
+	if res.Value != expected {
+		t.Errorf("obj wrong value. got: %t expected: %t", res.Value, expected)
+	}
 }
